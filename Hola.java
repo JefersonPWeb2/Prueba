ccc0c6d265b3dcde93faf84ff1b297c723f74d00
Jeferson <jbasurco@unsa.edu.pe>
2023-05-07 16:42:33 -0500

    Hola mundo

---
+++ Hola.java
@@ -0,0 +1,8 @@
+package hola;
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Bienvenidos a todos");
+        System.out.println("Mi nombre es Jeferson Joao Basurco Casani");
+    }
+}
