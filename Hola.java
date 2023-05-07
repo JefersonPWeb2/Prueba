8813bd629710c3c768839db018a3c104cbd40aaa
JefersonPWeb2 <132604488+JefersonPWeb2@users.noreply.github.com>
2023-05-07 16:47:59 -0500

    Delete Hola.java

--- Hola.java
+++
@@ -1,8 +0,0 @@
-package hola;
-public class Hola {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        System.out.println("Bienvenidos a todos");
-        System.out.println("Mi nombre es Jeferson Joao Basurco Casani");
-    }
-}
